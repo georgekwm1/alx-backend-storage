bee791d232ca77afc18813e049f195edcdbd5b65
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-08-29 17:26:18 +0100

    "Added a Python function to retrieve a list of students sorted by average score"

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,26 @@
+""" A Python function that  returns the list of school having a specific topic"""
+
+
+def top_students(mongo_collection):
+    '''
+    '''
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
