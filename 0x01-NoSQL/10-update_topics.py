c26c3555f2946d6a9e4fbf40c3ea75dfb503e076
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-08-29 14:31:32 +0100

    "Added 10-main.py and 10-update_topics.py files with MongoDB functionality"

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+""" A Python function that  changes all topics of a school document based on the name"""
+from pymongo import MongoClient
+import pprint
+
+
+def update_topics(mongo_collection, name, topics):
+    '''update topics of a school document based on the nam.
+    '''
+    result = mongo_collection.update_one(
+        {"name": name}, {"$set": {"topics": topics}})
